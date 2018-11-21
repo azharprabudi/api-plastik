e5ee4d76b92b35eba85b8596bbed7a26cb82bfcb
azharprabudi <azharprabui@gmail.com>
2018-11-21 23:28:47 +0700

    update error messages

--- errors/abstract.go
+++ errors/abstract.go
@@ -2,7 +2,8 @@ package errors
 
 // ErrorResponse ...
 type ErrorResponse struct {
-	Code       string `json:"code"`
-	Message    string `json:"message"`
-	StatusText string `json:"statusText"`
+	Code       string   `json:"code"`
+	Message    string   `json:"message"`
+	StatusText string   `json:"statusText"`
+	Fields     []string `json:"fields"`
 }
--- errors/error.go
+++ errors/error.go
@@ -1,10 +1,11 @@
 package errors
 
 // NewErrorReponse ...
-func NewErrorReponse(code string, message string, statusText string) *ErrorResponse {
+func NewErrorReponse(code string, message string, statusText string, fields []string) *ErrorResponse {
 	return &ErrorResponse{
 		Code:       code,
 		Message:    message,
 		StatusText: statusText,
+		Fields:     fields,
 	}
 }
