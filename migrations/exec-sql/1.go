c6cee913984bbb049a728e7bd322f986dbaea779
azharprabudi <azharprabui@gmail.com>
2018-11-27 21:26:30 +0700

    update sql, add expense type and expense table

--- migrations/exec-sql/1.go
+++ migrations/exec-sql/1.go
@@ -2,41 +2,60 @@ package execsql
 
 // First ...
 var First = `
-CREATE TABLE "itemCategory" (
-	"itemCategoryId" serial NOT NULL,
-	"itemCategoryName" varchar(100) NOT NULL,
-	"createdAt" timestamptz NOT NULL,
-	CONSTRAINT item_category_pk PRIMARY KEY ("itemCategoryId")
+CREATE TABLE "item_categories" (
+	"id" serial NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"created_at" timestamptz NOT NULL,
+	CONSTRAINT item_categories_pk PRIMARY KEY ("id")
 );
 
-CREATE TABLE "item" (
-	"itemId" char(36) NOT NULL,
-	"itemName" varchar(100) NOT NULL,
-	"itemCategoryId" int NOT NULL,
-	"createdAt" timestamptz NOT NULL,
-	CONSTRAINT item_pk PRIMARY KEY ("itemId")
+CREATE TABLE "items" (
+	"id" char(36) NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"category_id" int NOT NULL,
+	"created_at" timestamptz NOT NULL,
+	CONSTRAINT items_pk PRIMARY KEY ("id")
 );
 
-ALTER TABLE item 
-   ADD CONSTRAINT fk_item_category
-   FOREIGN KEY ("itemCategoryId") 
-   REFERENCES "itemCategory"("itemCategoryId");
+ALTER TABLE items
+   ADD CONSTRAINT fk_item_categories
+   FOREIGN KEY ("category_id") 
+   REFERENCES "item_categories"("id");
 
-CREATE TABLE "supplier" (
-	"supplierId" char(36) NOT NULL,
-	"supplierName" varchar(100) NOT NULL,
-	"supplierPhone" varchar(15) NULL,
-	"supplierAddress" text NULL,
-	"createdAt" timestamptz NOT NULL,
-	CONSTRAINT supplier_pk PRIMARY KEY ("supplierId")
+CREATE TABLE "suppliers" (
+	"id" char(36) NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"phone" varchar(15) NULL,
+	"address" text NULL,
+	"created_at" timestamptz NOT NULL,
+	CONSTRAINT suppliers_pk PRIMARY KEY ("id")
 );
 
-CREATE TABLE "seller" (
-	"sellerId" char(36) NOT NULL,
-	"sellerName" varchar(100) NOT NULL,
-	"sellerPhone" varchar(15) NULL,
-	"sellerAddress" text NULL,
-	"createdAt" timestamptz NOT NULL,
-	CONSTRAINT seller_pk PRIMARY KEY ("sellerId")
+CREATE TABLE "sellerss" (
+	"id" char(36) NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"phone" varchar(15) NULL,
+	"address" text NULL,
+	"created_at" timestamptz NOT NULL,
+	CONSTRAINT sellers_pk PRIMARY KEY ("id")
 );
+
+CREATE TABLE "expense_types" (
+	"id" serial NOT NULL,
+	"name" varchar(100) NOT NULL,
+	CONSTRAINT expense_types_pk PRIMARY KEY ("id")
+);
+
+CREATE TABLE "expenses" (
+	"id" serial NOT NULL,
+	"expense_type_id" INT NOT NULL,
+	"name" varchar(100) NOT NULL,
+	CONSTRAINT expenses_pk PRIMARY KEY ("id")
+);
+
+ALTER TABLE expenses
+   ADD CONSTRAINT fk_item_categories
+   FOREIGN KEY ("expense_type_id") 
+   REFERENCES "expense_types"("id");
+
 `
