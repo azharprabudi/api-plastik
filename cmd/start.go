d9955ba0f9a45d230353ccfb11fcbdc17adba623
azharprabudi <azharprabui@gmail.com>
2018-12-17 00:11:53 +0700

    add conf for heroku

--- cmd/start.go
+++ cmd/start.go
@@ -2,12 +2,20 @@ package cmd
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/azharprabudi/api-plastik/config"
 	"github.com/go-chi/chi"
 )
 
 // StartServer ...
-func StartServer(port string, r *chi.Mux) {
+func StartServer(r *chi.Mux) {
+	// get conf port
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = os.Getenv(config.Port)
+	}
+
 	err := http.ListenAndServe(port, r)
 	if err != nil {
 		panic(err)
